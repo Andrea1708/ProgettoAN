5de83c965922048039c55229506d6655a1fdae00
Andrea Belleggia <andreabelleggia99@gmail.com>
2020-10-21 13:38:40 +0200

    fix 5.1

--- ANSpringBootApp/src/main/java/OProject/ANSpringBootApp/JSON/JsonProcessing.java
+++ ANSpringBootApp/src/main/java/OProject/ANSpringBootApp/JSON/JsonProcessing.java
@@ -1,19 +1,18 @@
 package OProject.ANSpringBootApp.JSON;
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 
 import OProject.ANSpringBootApp.Model.States;
 import OProject.ANSpringBootApp.Service.URLservice;
@@ -48,16 +47,18 @@ public static JSONArray readURL() throws JSONException {
 		
 		
 public static void Parsing (JSONArray jsonArr, ArrayList<States> list){				
-		States objval = new States();
-		JSONObject json;
-		for(int i=0; i <jsonArr.length(); i++) 
+		
+		for(int i=0; i < jsonArr.length();i++) 
 		{
-			json= (JSONObject) jsonArr.getJSONObject(i);
+			States objval = new States();
+			JSONObject json;
+			json= (JSONObject) jsonArr.get(i);
 			objval.setCountry((String)json.get("Country"));
 			objval.setSlug((String)json.get("Slug"));
 			objval.setISO2((String)json.get("ISO2"));
 			list.add(objval);
 		}
+	
 		}
 
 }
