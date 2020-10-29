9426d0c05d142eb34fa403d0a35b11c40cf85b1a
Andrea Belleggia <andreabelleggia99@gmail.com>
2020-10-29 11:32:55 +0100

    Fix 7.7

--- ANSpringBootApp/src/main/java/OProject/ANSpringBootApp/Service/StatsService.java
+++ ANSpringBootApp/src/main/java/OProject/ANSpringBootApp/Service/StatsService.java
@@ -4,7 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+
 
 import OProject.ANSpringBootApp.Model.Nation;
 import OProject.ANSpringBootApp.Util.Stats;
